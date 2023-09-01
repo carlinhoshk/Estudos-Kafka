1a38d2024e1b950bf6ffa4a991a00de6c46ec917
carlinhoshk <carlosmdohk@gmail.com>
2023-08-31 21:59:13 -0300

    Feat: Listener implemented :white_check_mark:

---
+++ kafka-consumer/src/main/java/io/github/carlinhoshk/kafkaconsumer/listeners/KafkaListeners.java
@@ -0,0 +1,14 @@
+package io.github.carlinhoshk.kafkaconsumer.listeners;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaListeners {
+
+    @KafkaListener(topics = "teste-topic", groupId = "testeGroup")
+    public void listener(String data){
+        System.out.println("--------------------------------------------------------------");
+        System.out.println(String.format("{Consumer} - Menssagem recebida: %s \n", data));
+    }
+}
