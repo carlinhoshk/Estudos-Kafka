afe761c1e6d49755545ebcafd6645b3cba5dd515
carlinhoshk <carlosmdohk@gmail.com>
2023-09-01 01:03:23 -0300

    Chore: Fix topic name to testing .

--- kafka-consumer/src/main/java/io/github/carlinhoshk/kafkaconsumer/listeners/KafkaListeners.java
+++ kafka-consumer/src/main/java/io/github/carlinhoshk/kafkaconsumer/listeners/KafkaListeners.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaListeners {
-
-    @KafkaListener(topics = "teste-topic", groupId = "testeGroup")
+    /*
+    Criar e adicionar o repository de menssagem
+    @Autowired
+    private MessageRepository
+    */
+    @KafkaListener(topics = "test-topic", groupId = "testeGroup")
     public void listener(String data){
         System.out.println("--------------------------------------------------------------");
         System.out.println(String.format("{Consumer} - Messenger receiver: %s \n", data));
